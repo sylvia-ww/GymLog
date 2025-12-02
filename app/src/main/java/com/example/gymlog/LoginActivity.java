464073fba111557e8b8c17e272a98b95570039f4
sylvia-ww <168684863+sylvia-ww@users.noreply.github.com>
2025-12-02 01:28:48 -0800

    End of video 10

--- app/src/main/java/com/example/gymlog/LoginActivity.java
+++ app/src/main/java/com/example/gymlog/LoginActivity.java
@@ -4,14 +4,21 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.gymlog.database.GymLogRepository;
+import com.example.gymlog.database.entities.User;
 import com.example.gymlog.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
     private ActivityLoginBinding binding;
+
+    private GymLogRepository repository;
+
+    private User user = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +26,45 @@ public class LoginActivity extends AppCompatActivity {
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        repository = GymLogRepository.getRepository(getApplication());
+
         binding.loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = MainActivity.mainActivityIntentFactory(getApplicationContext(), 0);
-                startActivity(intent);
+                if(!verifyUser()) {
+                    toastMaker("Invalid credentials");
+                }else {
+                    Intent intent = MainActivity.mainActivityIntentFactory(getApplicationContext(), user.getId());
+                    startActivity(intent);
+                }
             }
         });
+    }
+
+    private boolean verifyUser(){
+        String username = binding.userNameLoginEditText.getText().toString();
+        if(username.isEmpty()) {
+            toastMaker("Username should not be blank");
+            return false;
+        }
+        user = repository.getUserByUserName(username);
+        if(user != null){
+            //verify password
+            String password = binding.passwordLoginEditText.getText().toString();
+            if( password.equals(user.getPassword())) {
+                return true;
+            }else{
+                toastMaker("Invalid password");
+                return false;
+            }
+        }
+        toastMaker(String.format("No %s found"
+                ,username));
+        return false;
+    }
+
+    private void toastMaker(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
     static Intent loginIntentFactory(Context context){
