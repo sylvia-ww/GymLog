0903fc13f69ef86b619f721d049a30c5aff76531
sylvia-ww <168684863+sylvia-ww@users.noreply.github.com>
2025-12-02 00:02:38 -0800

    End of video 8: Added a login screen

--- app/src/main/java/com/example/gymlog/MainActivity.java
+++ app/src/main/java/com/example/gymlog/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.gymlog;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
@@ -17,6 +19,8 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    //key for user id
+    private static final String MAIN_ACTIVITY_USER_ID = "com.example.gymlog.MAIN_ACTIVITY_USER_ID";
     private ActivityMainBinding binding;
     private GymLogRepository repository;
 
@@ -33,6 +37,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        loginUser();
+        
+        //if loggedInUserId is default, redirect to login screen
+        if(loggedInUserId == -1) {
+            Intent intent = LoginActivity.loginIntentFactory(getApplicationContext());
+            startActivity(intent);
+        }
 
         //make db
         repository = GymLogRepository.getRepository(getApplication());
@@ -59,6 +71,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void loginUser() {
+        //first time app loads -> no intent used to start MainActivity -> getIntent() return -1
+        //kicks us to sign-in page
+        loggedInUserId = getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID, -1);
+    }
+
+    static Intent mainActivityIntentFactory(Context context, int userId){
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(MAIN_ACTIVITY_USER_ID, userId);
+        return intent;
     }
 
     private void insertGymLogRecord() {
