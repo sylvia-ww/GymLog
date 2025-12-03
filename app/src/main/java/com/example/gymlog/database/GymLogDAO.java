0698e590d2555b66c4f042b655b9e792f0c60bc2
sylvia-ww <168684863+sylvia-ww@users.noreply.github.com>
2025-12-02 18:58:21 -0800

    End of Recycler view video

--- app/src/main/java/com/example/gymlog/database/GymLogDAO.java
+++ app/src/main/java/com/example/gymlog/database/GymLogDAO.java
@@ -22,4 +22,7 @@ public interface GymLogDAO {
 
     @Query("SELECT * FROM " + GymLogDatabase.GYM_LOG_TABLE + " WHERE userId = :loggedInUserId ORDER BY date DESC")
     List<GymLog> getRecordsbyUserId(int loggedInUserId);
+
+    @Query("SELECT * FROM " + GymLogDatabase.GYM_LOG_TABLE + " WHERE userId = :loggedInUserId ORDER BY date DESC")
+    LiveData<List<GymLog>> getRecordsbyUserIdLiveData(int loggedInUserId);
 }
--- app/src/main/java/com/example/gymlog/database/GymLogRepository.java
+++ app/src/main/java/com/example/gymlog/database/GymLogRepository.java
@@ -94,6 +94,10 @@ public class GymLogRepository {
         return userDAO.getUserByUserId(userId);
     }
 
+    public LiveData<List<GymLog>> getAllLogsByUserIdLiveData(int loggedInUserId){
+        return gymLogDAO.getRecordsbyUserIdLiveData(loggedInUserId);
+    }
+    @Deprecated
     public ArrayList<GymLog> getAllLogsByUserId(int loggedInUserId) {
         Future<ArrayList<GymLog>> future = GymLogDatabase.databaseWriteExecutor.submit(
                 new Callable<ArrayList<GymLog>>() {
